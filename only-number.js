c3a8861912d4ad58534a1f638e95f530486c7a35
ezfop <ezfop@hotmail.com>
2016-02-05 18:44:13 +0200

    Create only-number.js

---
+++ only-number.js
@@ -0,0 +1,22 @@
+<html>
+<script>
+    /* Created by Aydin Uzmez on 05.02.2016. */
+    function onlyNumber(element) {
+        var regex = /^\d+$/;
+        if (!regex.test(element.value)) {
+            document.getElementById(element.id).value = element.value.substring(0, element.value.length-1);
+        } else {
+         /* Code is here ;*/ alert("number came");
+        }
+
+    }
+</script>
+
+<body>
+<!-- Use -->
+<input type="text" id="abc" oninput="onlyNumber(this)"/>  <!--  //that must be id (abc) -->
+</body>
+
+
+
+</html>
